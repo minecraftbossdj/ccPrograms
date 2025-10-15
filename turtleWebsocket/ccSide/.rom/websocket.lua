3d6ade94551a283e869fd9916b849600dea7de68
DJ <minecraftbossdj@gmail.com>
2025-10-14 21:53:14 -0400

    Update websocket.lua

--- turtleWebsocket/ccSide/.rom/websocket.lua
+++ turtleWebsocket/ccSide/.rom/websocket.lua
@@ -1,15 +1,21 @@
-local ip = ""
+local file = fs.open(".rom/ip.txt","r")
+if file == nil then
+    error("no ip!")
+end
+
+local ip = file.readAll()
+file.close()
 
 _G.WS = http.websocket(ip..":5000")
 _G.WS2 = http.websocket(ip..":5001")
 
 if _G.WS == nil then
-    print("websocket did NOT connect. Retrying...")
+    print("websocket 1 did NOT connect. Retrying...")
     sleep(0.5)
     os.reboot()
 end
 if _G.WS2 == nil then
-    print("ws didnt connect, retrying")
+    print("Websocket 2 did NOT connect, Retrying...")
     sleep(0.5)
     os.reboot()
 end
