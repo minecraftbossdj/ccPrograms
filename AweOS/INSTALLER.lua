e040e880eeb86894bb9126a05aaa4ac8f8f6cf07
DJ <minecraftbossdj@gmail.com>
2025-12-03 02:04:31 -0500

    Simplify file downloads in INSTALLER.lua
    Refactored the installer to use a download function for multiple files.

--- AweOS/INSTALLER.lua
+++ AweOS/INSTALLER.lua
@@ -1,24 +1,26 @@
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/startup.lua startup.lua")
+local startURL = "https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/"
 
+local function download(url)
+    shell.run("wget "..startURL..url.." "..url)
+end
 
---apis
-fs.makeDir("AweOS/APIs/")
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/AweOS/APIs/roundAPI.lua AweOS/APIs/roundAPI.lua")
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/AweOS/APIs/sha2.lua AweOS/APIs/sha2.lua")
-shell.run("wget https://raw.githubusercontent.com/NullHarp/misc-cc-tweaked-programs/refs/heads/main/API/buttonAPI.lua AweOS/APIs/buttonAPI.lua")
-shell.run("wget https://raw.githubusercontent.com/NullHarp/misc-cc-tweaked-programs/refs/heads/main/API/windowAPI.lua AweOS/APIs/windowAPI.lua")
+local toDownload = {
+    "AweOS/boot.lua",
+    "AweOS/login.lua",
+    "AweOS/APIs/Menyuu.lua",
+    "AweOS/APIs/filesystem.lua",
+    "AweOS/APIs/minilogger.lua",
+    "AweOS/APIs/sha2.lua",
+    "AweOS/APIs/thread.lua",
+    "AweOS/APIs/util.lua",
+    "AweOS/programs/fileExplorer.lua",
+    "AweOS/programs/multishell.lua",
+    "AweOS/programs/taskManager.lua",
+    "AweOS/programs/aukit/aukit.lua",
+    "AweOS/programs/aukit/austream.lua",
+    "AweOS/users/admin/password.txt"
+}
 
---configs
-fs.makeDir("AweOS/configs/")
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/AweOS/configs/quotes.txt AweOS/configs/quotes.txt")
-
---users
-fs.makeDir("AweOS/users/admin")
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/AweOS/users/admin/pass.txt AweOS/users/admin/pass.txt")
-
---main files
-fs.makeDir("AweOS/")
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/AweOS/AweOS.lua AweOS/AweOS.lua")
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/AweOS/boot.lua AweOS/boot.lua")
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/AweOS/randomQuote.lua AweOS/randomQuote.lua")
-shell.run("wget https://raw.githubusercontent.com/minecraftbossdj/ccPrograms/refs/heads/main/AweOS/AweOS/AweOS/PasswordSys.lua AweOS/PasswordSys.lua")
+for _, v in pairs(toDownload) do
+    download(v)
+end
